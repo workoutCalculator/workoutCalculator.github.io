0ce9dc4e8f54c6e7a07c7068d0f2dec8a63a05d0
Jacob McCarthy <jnmc98@googlemail.com>
2025-05-29 11:46:27 +0100

    Fixed AMRAP functionality with improved weight mulitple caluclation. Small improvements to visual styling also.

--- calculator.js
+++ calculator.js
@@ -1,41 +1,47 @@
 import { timePerRep } from './timePerRep.js';
 
-// constans
+// constants
+const beginnerAthlete = 2;
 const averageAthlete = 1.25;
-const strongerAthlete = 0.8;
+const strongerAthlete = 0.87;
 const strongErg = 0.95;
 const averageErg = 1.15;
+const beginnerErg = 1.5;
+
+// global variables
+var athleteMutliplier = {
+    strong: 1,
+    average: 1,
+    beginner: 1
+};
+
+// text constants
+const notes = `Cardio machine and ERG machines are assumed to be in meters unless specified with <strong>cals</strong> in the movement name. See example above. Please include weight in <strong>kg</strong>. <strong style="color: pink;">Please do not use '-' in movements, this can only be used for rep ladders (e.g. '21-15-9).</strong> See example below:
+<ul>
+    <li>21-15-9</li>
+    <li>pull ups</li>
+    <li>40kg thrusters</li>
+</ul>`;
 
 document.getElementById('workout-form').addEventListener('submit', function (e) {
     e.preventDefault();
 
-    var isBodyweight = false;
-    var isWeighted = false;
-    var isGymnastics = false;
-    var isCore = false;
-    var isMisc = false;
-    var isErg = false;
-
     const type = document.getElementById('type').value;
     const rounds = document.getElementById('rounds').value || 1;
-    const timeCap = document.getElementById('time-cap');
-    const roundsVal = parseInt(document.getElementById('rounds').value);
-    const timeCapVal = parseFloat(document.getElementById('time-cap').value) || null;
+    const timeCap = document.getElementById('time-cap').value || 15;
     const rawMovements = document.getElementById('movements').value.trim().split('\n');
     const teamSize = document.getElementById('teamInput').value || 1;
     const teamType = document.querySelector('input[name="workoutType"]:checked').value;
 
     let totalTime = 0;
-    var teamMultiplier = 1;         // changes depending on chipper team size
-    var weightMultiplier = 1;       // changes depending on weight of movement
     var restMultiplier = 1.1;       // changes depending on rest time
-    var athleteMutliplier = {
-        strong: 1,
-        average: 1
-    };
-    var timeIntervals = [];           // set default value to 0 min
-    var timeIntervalTotals = [[]];    // array to store time intervals for time based workouts
+    athleteMutliplier.strong = strongerAthlete;
+    athleteMutliplier.beginner = beginnerAthlete;
+    athleteMutliplier.average = averageAthlete;
+    var timeIntervals = [];         // set default value to 0 min
+    var timeIntervalTotals = [[]];  // array to store time intervals for time based workouts
     let workoutSegments = [];       // array to store workout segments for teamTime workouts
+    var ladderParts = [];           // array to store ladder parts for ladder workouts, if exists
 
     if (teamSize > 1 && teamType === 'team') {
         teamMultiplier = 0.85 + 0.15*(1 / teamSize);
@@ -44,9 +50,7 @@ document.getElementById('workout-form').addEventListener('submit', function (e) 
     console.log('rawMovements', document.getElementById('movements').value.trim().split('\n'));
     for (let line of rawMovements) {
 
-        var time = 1; // set default value to 1 second   
-        var reps = 0;
-        if (line.split(',').map(s => s.trim()).length < 2) {
+        if (line.split(',').map(s => s.trim()).length < 2 && ladderParts.length === 0) {
             // check if is timer
             if (teamType === "teamTime" && line.includes('@')) {
                 // this is a time indicator
@@ -59,131 +63,44 @@ document.getElementById('workout-form').addEventListener('submit', function (e) 
                 timeIntervals.push(parseFloat(line.split('@')[1].trim()));
                 timeIntervalTotals.push([]);
             }
+            if (line.includes('-')) {
+                // must be a ladder workout
+                ladderParts = line.split('-').map(s => s.trim());
+            }
 
             // if line does not contain a comma, skip it
             console.log('skipping line', line);
             continue;
         }
-        var [repsStr, movement] = line.split(',').map(s => s.trim());
-        // if string contains 'km', replace it with '' and mulitply reps by 1000
-        if (repsStr.includes('km')) {
-            repsStr = repsStr.replace(/km/g, '');
-            reps = parseInt(repsStr);
-            reps *= 1000;
-        } else {
-            repsStr = repsStr.replace(/m/g, '');
-            reps = parseInt(repsStr);
-        }
-        movement = movement.toLowerCase();
-        // also replace all instances of '-' with ' ' in the movement name
-        movement = movement.replace(/-/g, ' ');
-
-        // extract weight from movenment if exists
-        const weight = extractWeight(movement) || null;
-        // remove weight from movement name
-        if (weight) {
-            movement = movement.replace(/(\d+(\.\d+)?)\s*kg/i, '').trim();
-        }
-
-        // loop through timePerRep and check if movement exists
-        for (const key in timePerRep) {
-
-            console.log('key', key);
-            const value = timePerRep[key];
-            // check if value is an object
-            if (timePerRep[key][movement] || timePerRep[key][movement.slice(0, -1)]) {
-                // if movement exists, break out of loop
-                time = timePerRep[key][movement];
-                if (key === 'weighted') {
-                    // multiplier is determined by weight
-                    console.log("we here");
-                    console.log('weight', weight);
-                    if (weight > 45 && weight <= 70) {
-                        weightMultiplier = 1.5;
-                    }
-                    else if (weight > 70) {
-                        weightMultiplier = 2.5;
-                    } else {
-                        weightMultiplier = 1;
-                    }
-                    console.log('weight', weight);
-
-                } else {
-                    weightMultiplier = 1;
+        // check ladder workout or not
+        // if so, find correct reps for each part
+        var [repsStr, movement] = [];
+        if (ladderParts.length !== 0) {
+            /* Must perform alternate time calc based on ladder structure */
+            for (let i = 0; i < ladderParts.length; i++) {
+                for (let j = 1; j < rawMovements.length; j++) {
+                    var tempTime = 0;
+                    [tempTime, restMultiplier] = findTotalTime(ladderParts[i], rawMovements[j].trim(),teamType, workoutSegments, timeIntervals, timeIntervalTotals);
+                    totalTime += tempTime;
                 }
-
-                switch (key) {
-                    case 'bodyweight':
-                        isBodyweight = true;
-                        break;
-                    case 'weighted':
-                        isWeighted = true;
-                        break;
-                    case 'gymnastics':
-                        isGymnastics = true;
-                        break;
-                    case 'core':
-                        isCore = true;
-                        break;
-                    case 'misc':
-                        isMisc = true;
-                        break;
-                    case 'erg':
-                        isErg = true;
-                        break;
-                }
-                break;
-            } else {
-                time = 1;
             }
-        }
-        if (isErg && !isBodyweight && !isWeighted && !isGymnastics && !isCore && !isMisc) {
-            // if erg and noting else, set multiplier to 1
-            var sprintFactor = 1;
-            if (reps <= 500) {
-                sprintFactor = 0.95;
-                restMultiplier = 1;
-            } else if (reps > 500 && reps < 1000) {
-                sprintFactor = 1;
-                restMultiplier = 1;
-            } else {
-                sprintFactor = 1.05;
-            }
-            time *= sprintFactor;
-            athleteMutliplier.strong = strongErg;
-            athleteMutliplier.average = averageErg;
+            break;
         } else {
-            athleteMutliplier.strong = strongerAthlete;
-            athleteMutliplier.average = averageAthlete;
+            [repsStr, movement] = line.split(',').map(s => s.trim());
         }
-        console.log('movement', movement);
-        console.log('time', time);
-        console.log('reps', reps);
-        console.log('multiplier', weightMultiplier);
-        if (teamType === 'teamTime') {
-            // also if no time interval is set, then default to previous time interval, if none, then default to 0min
-            if (timeIntervals.length === 0) {
-                timeIntervals.push(0);
-            }
-           // need to append to time in intervals array
-            timeIntervalTotals[timeIntervalTotals.length - 1].push(reps * weightMultiplier * time);
-            workoutSegments.push({
-                startTime: timeIntervals[timeIntervals.length - 1],
-                type: movement,
-                distance: reps
-            });
-        }
-        else {
-            totalTime += reps * weightMultiplier * teamMultiplier * time;
-        }
+
+        [tempTime, restMultiplier] = findTotalTime(repsStr, movement, teamType, workoutSegments, timeIntervals, timeIntervalTotals);
+        totalTime += tempTime;
     }
+        
     if (teamType === 'teamTime') {
         timeIntervalTotals.shift();
     }
 
-    if (type !== 'emom') {
+    if (type !== 'emom' && type !== 'amrap') {
         totalTime *= rounds;
     }
+
     if (teamType === 'teamTime') {
         totalTime = estimateWorkoutTime({workoutSegments, timePerRep, teamSize});
     }
@@ -197,21 +114,37 @@ document.getElementById('workout-form').addEventListener('submit', function (e) 
         athelete = 'athletes';
     }
 
+    // case for amrap, must determine the rounds done
+    if (type === 'amrap') {
+        const timePerRound = totalTime;
+        const totalRounds = Math.round((timeCap * 60 / timePerRound) * 10) / 10;
+        if (totalRounds < 1) {
+            message = `You will not complete a single round in ${timeCap} minutes.`;
+        } else {
+            message = `Estimated rounds in ${timeCap} minutes:
+            Strong ${athelete}: ${Math.round(totalRounds / athleteMutliplier.strong * 10) / 10} rounds
+            Good ${athelete}: ${totalRounds} rounds
+            Average ${athelete}: ${Math.round(totalRounds / athleteMutliplier.average * 10) / 10} rounds
+            Beginner ${athelete}: ${Math.round(totalRounds / athleteMutliplier.beginner * 10) / 10} rounds`;
+    }
+        document.getElementById('results').innerText = message;
+        return;
+    }
+
     switch (type) {
         case 'for-time':
         message = `Estimated durations:
         Strong ${athelete}: ${Math.floor(totalTime * athleteMutliplier.strong / 60)} minutes ${Math.floor((totalTime * athleteMutliplier.strong) % 60)} seconds
         Good ${athelete}: ${Math.floor(totalTime / 60)} minutes ${Math.floor((totalTime) % 60)} seconds
-        Average ${athelete}: ${Math.floor(totalTime * athleteMutliplier.average / 60)} minutes ${Math.floor((totalTime * athleteMutliplier.average) % 60)} seconds`;
-        break;
-        case 'amrap':
-        message = `Estimated total reps in ${timeCap} minutes: ${Math.floor((timeCap * 60) / (totalTime / rounds)) * rawMovements.length}. Total Rounds: ${Math.floor((timeCap * 60) / (totalTime / rounds))}`;
+        Average ${athelete}: ${Math.floor(totalTime * athleteMutliplier.average / 60)} minutes ${Math.floor((totalTime * athleteMutliplier.average) % 60)} seconds
+        Beginner ${athelete}: ${Math.floor(totalTime * athleteMutliplier.beginner / 60)} minutes ${Math.floor((totalTime * athleteMutliplier.beginner) % 60)} seconds`;
         break;
         case 'emom':
         message = `Estimated round time: 
         Strong ${athelete}: ${Math.floor(totalTime / 60 * athleteMutliplier.strong)} minutes and ${Math.ceil((totalTime * athleteMutliplier.strong) % 60)} seconds
         Good ${athelete}: ${Math.floor(totalTime / 60)} minutes and ${Math.ceil((totalTime) % 60)} seconds
-        Average ${athelete}: ${Math.floor(totalTime / 60 * athleteMutliplier.average)} minutes and ${Math.ceil((totalTime * athleteMutliplier.average) % 60)} seconds`;
+        Average ${athelete}: ${Math.floor(totalTime / 60 * athleteMutliplier.average)} minutes and ${Math.ceil((totalTime * athleteMutliplier.average) % 60)} seconds
+        Beginner ${athelete}: ${Math.floor(totalTime / 60 * athleteMutliplier.beginner)} minutes and ${Math.ceil((totalTime * athleteMutliplier.beginner) % 60)} seconds`;
         break;
         case 'time-cap':
         message = totalTime / 60 > timeCap
@@ -246,6 +179,10 @@ function updatePage() {
     const individualExample = document.getElementById('individualExample');
     const teamExample = document.getElementById('teamExample');
     const teamTimeExample = document.getElementById('teamTimeExample');
+    
+    // set notes
+    document.querySelectorAll('.notes').forEach(p => {
+    p.innerHTML = notes;});
 
     // Hide all elements by default
     rounds.classList.add('hidden');
@@ -341,7 +278,6 @@ function updatePage() {
             }
         }
     });
-
 }
 
 function extractWeight(input) {
@@ -445,3 +381,145 @@ document.getElementById('feedback-form').addEventListener('submit', async functi
     document.getElementById('feedbackResult').textContent = "Network error. Please try again.";
   }
 });
+
+function findTotalTime(repsStr, movement, teamType, workoutSegments = [], timeIntervals = [], timeIntervalTotals = [[]]) {
+
+    // Initialize variables
+    var isBodyweight = false;
+    var isWeighted = false;
+    var isGymnastics = false;
+    var isCore = false;
+    var isMisc = false;
+    var isErg = false;
+
+    var restMultiplier = 1.1;        // changes depending on rest time
+    var teamMultiplier = 1;         // changes depending on chipper team size
+    var reps = 0;
+    var time = 1;                   // set default value to 1 second
+    let totalTime = 0;
+    var weightMultiplier = 1;       // changes depending on weight of movement
+
+    // if string contains 'km', replace it with '' and mulitply reps by 1000
+    if (repsStr.includes('km')) {
+        repsStr = repsStr.replace(/km/g, '');
+        reps = parseInt(repsStr);
+        reps *= 1000;
+    } else {
+        repsStr = repsStr.replace(/m/g, '');
+        reps = parseInt(repsStr);
+    }
+    movement = movement.toLowerCase();
+    // also replace all instances of '-' with ' ' in the movement name
+    movement = movement.replace(/-/g, ' ');
+
+    // extract weight from movenment if exists
+    const weight = extractWeight(movement) || null;
+    // remove weight from movement name
+    if (weight) {
+        movement = movement.replace(/(\d+(\.\d+)?)\s*kg/i, '').trim();
+    }
+
+    // loop through timePerRep and check if movement exists
+    for (const key in timePerRep) {
+
+        console.log('key', key);
+        const value = timePerRep[key];
+        // check if value is an object
+        if (timePerRep[key][movement] || timePerRep[key][movement.slice(0, -1)]) {
+            // if movement exists, break out of loop
+            if (timePerRep[key][movement]) {
+                time = timePerRep[key][movement];
+            } else {
+                // if movement ends with 's', remove it
+                time = timePerRep[key][movement.slice(0, -1)];
+            }
+            if (key === 'weighted') {
+                // multiplier is determined by weight
+                weightMultiplier = calculateWeightMultiplier(weight);
+            } else {
+                weightMultiplier = 1;
+            }
+
+            switch (key) {
+                case 'bodyweight':
+                    isBodyweight = true;
+                    break;
+                case 'weighted':
+                    isWeighted = true;
+                    break;
+                case 'gymnastics':
+                    isGymnastics = true;
+                    break;
+                case 'core':
+                    isCore = true;
+                    break;
+                case 'misc':
+                    isMisc = true;
+                    break;
+                case 'erg':
+                    isErg = true;
+                    break;
+            }
+            break;
+        } else {
+            time = 1;
+        }
+    }
+
+    if (isErg && !isBodyweight && !isWeighted && !isGymnastics && !isCore && !isMisc) {
+        // if erg and noting else, set multiplier to 1
+        var sprintFactor = 1;
+        if (reps <= 500) {
+            sprintFactor = 0.95;
+            restMultiplier = 1;
+        } else if (reps > 500 && reps < 1000) {
+            sprintFactor = 1;
+            restMultiplier = 1;
+        } else {
+            sprintFactor = 1.05;
+        }
+        time *= sprintFactor;
+        athleteMutliplier.strong = strongErg;
+        athleteMutliplier.average = averageErg;
+        athleteMutliplier.beginner = beginnerErg;
+    }
+
+    if (teamType === 'teamTime') {
+        // also if no time interval is set, then default to previous time interval, if none, then default to 0min
+        if (timeIntervals.length === 0) {
+            timeIntervals.push(0);
+        }
+        // need to append to time in intervals array
+        timeIntervalTotals[timeIntervalTotals.length - 1].push(reps * weightMultiplier * time);
+        workoutSegments.push({
+            startTime: timeIntervals[timeIntervals.length - 1],
+            type: movement,
+            distance: reps
+        });
+    }
+    else {
+        totalTime = reps * weightMultiplier * teamMultiplier * time;
+    }
+    
+    return [totalTime, restMultiplier];
+}
+
+function calculateWeightMultiplier(weight) {
+    var base = 1; 
+
+    function sigmoid(x, midpoint, steepness) {
+        return 1 / (1 + Math.exp(-steepness * (x - midpoint)));
+    }
+
+    if (weight < 45) {
+        return 1;
+    } else if (weight <= 70) {
+        // Sigmoid from 1 to 1.5
+        base = sigmoid(weight, 57.5, 0.3); // Midpoint = (45 + 70) / 2
+        return 1 + (0.6 * base); // Scales from 1 to ~1.5
+    } else {
+        // Sigmoid from 1.5 to 2
+        base = sigmoid(weight, 85, 0.8); // Midpoint = shifted to fit upper range
+        return 1.6 + (1 * base); // Scales from 1.5 to ~2
+    }
+}
--- timePerRep.js
+++ timePerRep.js
@@ -13,7 +13,7 @@ export const timePerRep = {
     "burpee": 2,
     "get up": 1.75,
     "air squat": 0.9,
-    "pull up": 0.9,
+    "pull up": 0.8,
     "chest to bar": 1.1,
     "sit up": 0.8,
     "toes to bar": 1,
@@ -41,7 +41,7 @@ export const timePerRep = {
     "overhead squat": 1,
     "push press": 0.8,
     "push jerk": 0.9,
-    "thruster": 1,
+    "thruster": 1.9,
     "dumbbell snatch": 1.1,
     "dumbbell clean and jerk": 2,
     "kettlebell swing": 1,
@@ -59,7 +59,7 @@ export const timePerRep = {
     "muscle up": 3,
     "handstand walk": 0.5, // per step
     "pistol squat": 1,
-    "hollow rock": 0.6
+    "hollow rock": 0.7
     },
 
     // Conditioning / cardio machines
@@ -71,14 +71,14 @@ export const timePerRep = {
     "c2": 0.1, 
     "cal bike": 3,
     "cal c2": 3,
-    "run": 0.20, 
+    "run": 0.21, 
     "assault bike": 0.1, 
     "cal assault bike": 3,
     "ski": 0.25, 
     "cal ski": 2.85,
     "double under": 0.66,
     "single under": 0.8,
-    "box step-over": 2.3,
+    "box step-over": 2.5,
     "db box step-over": 3.5
     },
 
@@ -95,7 +95,7 @@ export const timePerRep = {
     "sandbag clean": 4,
     "sled push": 0.3,
     "farmer carry": 0.5,
-    "wall ball": 1.5,
+    "wall ball": 2,
     "devil's press": 3
     }
 };
